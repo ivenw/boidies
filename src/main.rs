513220cd848e36ef8763051703a075069382f267
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-09-10 11:39:14 +0200

    getting my boidies online

--- src/main.rs
+++ src/main.rs
@@ -28,17 +28,12 @@ fn main() {
         .add_startup_system_set(
             SystemSet::new()
                 .with_system(setup_camera)
-                .with_system(setup_bird),
+                .with_system(setup_boids),
         )
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                // .with_system(update_bird_velocity)
-                .with_system(move_boid_towards_perceived_scenter_of_mass)
-                .with_system(keep_distance_from_boids)
-                .with_system(move_boids_to_mouse)
-                // .with_system(match_other_boids_velocity)
-                .with_system(apply_velocity),
+                .with_system(boid_system),
         )
         // .add_system(update_scoreboard)
         .add_system(bevy::window::close_on_esc)
@@ -51,7 +46,7 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle::default());
 }
 
-fn setup_bird(mut commands: Commands) {
+fn setup_boids(mut commands: Commands) {
     let mut rng = rand::thread_rng();
     for _ in 0..NUM_BOIDS {
         commands
@@ -80,88 +75,90 @@ fn setup_bird(mut commands: Commands) {
     }
 }
 
-fn apply_velocity(mut query: Query<(&mut Transform, &mut Velocity)>) {
-    for (mut transform, mut velocity) in &mut query {
-        if velocity.length() > BOID_SPEED {
-            velocity.0 = velocity.0.normalize() * BOID_SPEED;
-        }
-        transform.translation.x += velocity.x * TIME_STEP;
-        transform.translation.y += velocity.y * TIME_STEP;
-    }
-}
+fn boid_system(mut query: Query<(Entity, &mut Transform, &mut Velocity), With<Boid>>) {}
 
-fn move_boid_towards_perceived_scenter_of_mass(
-    mut query: Query<(Entity, &Transform, &mut Velocity), With<Boid>>,
-) {
-    let boids: Vec<(Entity, Vec3)> = query.iter().map(|x| (x.0, x.1.translation)).collect();
-    for (entity, transform, mut velocity) in &mut query {
-        let mut arr = Vec3::new(0.0, 0.0, 0.0);
-        let mut len: u32 = 0;
-        for (e, translation) in boids.to_owned() {
-            if e != entity && transform.translation.distance(translation) < BOID_SENSING_RADIUS {
-                arr += translation;
-                len += 1;
-            }
-        }
-        let perceived_center = arr / len as f32;
-        let direction = Vec2::new(
-            perceived_center.x - transform.translation.x,
-            perceived_center.y - transform.translation.y,
-        )
-        .normalize();
-        velocity.x += direction.x * BOID_ACCELERATION * 0.5;
-        velocity.y += direction.y * BOID_ACCELERATION * 0.5;
-    }
-}
-
-fn keep_distance_from_boids(mut query: Query<(Entity, &Transform, &mut Velocity), With<Boid>>) {
-    let boids: Vec<(Entity, Vec3)> = query.iter().map(|x| (x.0, x.1.translation)).collect();
-    for (entity, transform, mut velocity) in &mut query {
-        for (e, translation) in boids.to_owned() {
-            if e != entity {
-                if transform.translation.distance(translation) < MIN_BOID_DIST {
-                    let direction =
-                        (1.0 * (transform.translation - translation)).normalize() * BOID_SPEED;
-                    velocity.x += direction.x * BOID_ACCELERATION * 0.1;
-                    velocity.y += direction.y * BOID_ACCELERATION * 0.1;
-                }
-            }
-        }
-    }
-}
-
-// fn match_other_boids_velocity(
-//     mut query: Query<(Entity, &mut Velocity), With<Boid>>,
-//     query2: Query<(Entity, &Velocity), With<Boid>>,
-// ) {
-//     // let boids: Vec<(Entity, &Velocity)> = query.iter().map(|x| (x.0, x.1)).collect();
-//     for (entity, mut velocity) in &mut query {
-//         let mut arr = Vec2::new(0.0, 0.0);
-//         let mut len: u32 = 0;
-//         for (e, vel) in &query2 {
-//             if e != entity {
-//                 arr.x += vel.x;
-//                 arr.y += vel.y;
-//                 len += 1;
-//             }
+// fn apply_velocity(mut query: Query<(&mut Transform, &mut Velocity)>) {
+//     for (mut transform, mut velocity) in &mut query {
+//         if velocity.length() > BOID_SPEED {
+//             velocity.0 = velocity.0.normalize() * BOID_SPEED;
 //         }
-//         let perceived_velocity = arr / len as f32;
-//         velocity.x += perceived_velocity.x / 8.0;
-//         velocity.y += perceived_velocity.y / 8.0;
+//         transform.translation.x += velocity.x * TIME_STEP;
+//         transform.translation.y += velocity.y * TIME_STEP;
 //     }
 // }
 
-fn move_boids_to_mouse(
-    mut query: Query<(&Transform, &mut Velocity), With<Boid>>,
-    mouse: Res<MousePosWorld>,
-) {
-    let mouse_pos = Vec2::new(mouse[0], mouse[1]);
-    // let pos = Vec2::new(0.0, 0.0);
-    for (transform, mut velocity) in &mut query {
-        let boid_pos = Vec2::new(transform.translation.x, transform.translation.y);
-        velocity.0 += (mouse_pos - boid_pos).normalize() * BOID_ACCELERATION;
-    }
-}
+// fn move_boid_towards_perceived_scenter_of_mass(
+//     mut query: Query<(Entity, &Transform, &mut Velocity), With<Boid>>,
+// ) {
+//     let boids: Vec<(Entity, Vec3)> = query.iter().map(|x| (x.0, x.1.translation)).collect();
+//     for (entity, transform, mut velocity) in &mut query {
+//         let mut arr = Vec3::new(0.0, 0.0, 0.0);
+//         let mut len: u32 = 0;
+//         for (e, translation) in boids.to_owned() {
+//             if e != entity && transform.translation.distance(translation) < BOID_SENSING_RADIUS {
+//                 arr += translation;
+//                 len += 1;
+//             }
+//         }
+//         let perceived_center = arr / len as f32;
+//         let direction = Vec2::new(
+//             perceived_center.x - transform.translation.x,
+//             perceived_center.y - transform.translation.y,
+//         )
+//         .normalize();
+//         velocity.x += direction.x * BOID_ACCELERATION * 0.5;
+//         velocity.y += direction.y * BOID_ACCELERATION * 0.5;
+//     }
+// }
+
+// fn keep_distance_from_boids(mut query: Query<(Entity, &Transform, &mut Velocity), With<Boid>>) {
+//     let boids: Vec<(Entity, Vec3)> = query.iter().map(|x| (x.0, x.1.translation)).collect();
+//     for (entity, transform, mut velocity) in &mut query {
+//         for (e, translation) in boids.to_owned() {
+//             if e != entity {
+//                 if transform.translation.distance(translation) < MIN_BOID_DIST {
+//                     let direction =
+//                         (1.0 * (transform.translation - translation)).normalize() * BOID_SPEED;
+//                     velocity.x += direction.x * BOID_ACCELERATION * 0.1;
+//                     velocity.y += direction.y * BOID_ACCELERATION * 0.1;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// // fn match_other_boids_velocity(
+// //     mut query: Query<(Entity, &mut Velocity), With<Boid>>,
+// //     query2: Query<(Entity, &Velocity), With<Boid>>,
+// // ) {
+// //     // let boids: Vec<(Entity, &Velocity)> = query.iter().map(|x| (x.0, x.1)).collect();
+// //     for (entity, mut velocity) in &mut query {
+// //         let mut arr = Vec2::new(0.0, 0.0);
+// //         let mut len: u32 = 0;
+// //         for (e, vel) in &query2 {
+// //             if e != entity {
+// //                 arr.x += vel.x;
+// //                 arr.y += vel.y;
+// //                 len += 1;
+// //             }
+// //         }
+// //         let perceived_velocity = arr / len as f32;
+// //         velocity.x += perceived_velocity.x / 8.0;
+// //         velocity.y += perceived_velocity.y / 8.0;
+// //     }
+// // }
+
+// fn move_boids_to_mouse(
+//     mut query: Query<(&Transform, &mut Velocity), With<Boid>>,
+//     mouse: Res<MousePosWorld>,
+// ) {
+//     let mouse_pos = Vec2::new(mouse[0], mouse[1]);
+//     // let pos = Vec2::new(0.0, 0.0);
+//     for (transform, mut velocity) in &mut query {
+//         let boid_pos = Vec2::new(transform.translation.x, transform.translation.y);
+//         velocity.0 += (mouse_pos - boid_pos).normalize() * BOID_ACCELERATION;
+//     }
+// }
 
 fn cursor_grab_system(
     mut windows: ResMut<Windows>,
